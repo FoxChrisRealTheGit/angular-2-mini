b5bc290db39b092287e70fa9aad8756a64973e52
FoxChrisRealTheGit <cfox1290@gmail.com>
2018-02-07 13:29:54 -0700

    finished assignment

--- js/products/productsSrvc.js
+++ js/products/productsSrvc.js
@@ -1,3 +1,12 @@
-angular.module('myApp').service('productsSrvc', function() {
-
+angular.module('myApp').service('productsSrvc', function($http) {
+    this.getShoeData = function(){
+        return $http.get('https://practiceapi.devmountain.com/products?category=shoes').then(res=>{
+            return res.data.results
+        })
+    }
+    this.getSockData = function(){
+        return $http.get('https://practiceapi.devmountain.com/products?category=socks').then(res=>{
+            return res.data.results
+        })
+    }
 });
